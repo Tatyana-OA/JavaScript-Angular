891f9cdecaa68e89d57849b07554b6162e1ad50c
Tatyana Asenova <tasenova@devrix.com>
2021-07-27 21:19:52 +0300

    Added environments optimisation

--- my-app/src/app/content.service.ts
+++ my-app/src/app/content.service.ts
@@ -2,6 +2,10 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IPost, ITheme } from './shared/interfaces';
 
+import {environment} from '../environments/environment';
+const API_URL = environment.apiUrl;
+//see environments
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,11 +14,11 @@ export class ContentService {
   constructor(private http: HttpClient) { }
 
   loadThemes() {
-	return this.http.get<ITheme[]>('http://localhost:3000/api/themes');
+	return this.http.get<ITheme[]>(`${API_URL}/themes`);
   }
 
   loadPosts(limit?: number) {
 	  const query = limit ? `?limit=${limit}` : '';
-	return this.http.get<IPost[]>(`http://localhost:3000/api/posts${query}`);
+	return this.http.get<IPost[]>(`${API_URL}/posts${query}`);
 }
 }
